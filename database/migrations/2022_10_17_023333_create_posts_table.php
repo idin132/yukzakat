e3413fafc04ac591ac732e751d2b73b14bec9a9f
Idin Naufal Hakim <idinnaufal131204@gmail.com>
2022-10-17 13:07:01 +0700

    perbaikan

--- database/migrations/2022_10_17_023333_create_posts_table.php
+++ database/migrations/2022_10_17_023333_create_posts_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('username');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -47,6 +47,14 @@
                 </p>
             </a>
         </li>
+        <li class="nav-item">
+            <a href="{{ route('pembayaranp.index') }}" class="nav-link">
+                <i class="nav-icon bi bi-people-fill"></i>
+                <p>
+                    Posts
+                </p>
+            </a>
+        </li>
 
     </ul>
 </nav>
